bf21c8dbe6d80843bf4a35e3acc7678df88d095d
kookywarrior <kookywarrior@gmail.com>
2026-03-17 22:32:25 +0800

    moved tmp special ignore case when edit filename from small to capitalized filename, edited tsconfig

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
-import { StrictMode } from 'react' // Added for better debugging
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App' // Matches capital 'A' filename
+import App from './App'
 import './index.css'
 
-// We store the element in a variable to keep it clean
 const rootElement = document.getElementById('root')
 
 if (!rootElement) throw new Error('Failed to find the root element')
